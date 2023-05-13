ca252eadad99fbe477cc40a1767724394e264c4d
DorTeri <dortayari@gmail.com>
2023-05-13 21:36:22 +0300

    Added emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,11 +19,32 @@ const Contact = () => {
   const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
+    const { name, value } = e.target
 
+    setForm({ ...form, [name]: value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
+    setLoading(true)
+
+    try {
+      await emailjs.send('service_9xj4wpx',
+        'template_pj2s1bu',
+        {
+          from_name: form.name,
+          to_name: 'Dor',
+          from_email: form.email,
+          to_email: 'dortayari@gmail.com',
+          message: form.message
+        },
+        '4YkYELc0xaJr0s8ol')
+      setLoading(false)
+    } catch (err) {
+      setLoading(false)
+      console.log(err)
+    }
   }
 
 
@@ -41,39 +62,39 @@ const Contact = () => {
           ref={formRef}
           onSubmit={handleSubmit}
           className="mt-12 flex flex-col gap-8">
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Name</span>
-              <input type="text" name="name" value={form.name} 
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input type="text" name="name" value={form.name}
               onChange={handleChange} placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary
                text-white rounded-lg outlined-none border-none font-medium"/>
-            </label>
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Email</span>
-              <input type="email" name="email" value={form.email} 
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input type="email" name="email" value={form.email}
               onChange={handleChange} placeholder="What's your email?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary
                text-white rounded-lg outlined-none border-none font-medium"/>
-            </label>
-            <label className="flex flex-col">
-              <span className="text-white font-medium mb-4">Your Message</span>
-              <textarea rows='7' name="message" value={form.message} 
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea rows='7' name="message" value={form.message}
               onChange={handleChange} placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary
                text-white rounded-lg outlined-none border-none font-medium"/>
-            </label>
+          </label>
 
-            <button type="submit"
+          <button type="submit"
             className="bg-tertiary py-3 px-8 outline-none rounded-xl
             w-fit text-white font-bold shadow-md shadow-primary">
-              {loading ? 'Sending...' : 'Send'}
-            </button>
+            {loading ? 'Sending...' : 'Send'}
+          </button>
         </form>
       </motion.div>
 
       <motion.div
-      variants={slideIn('right', 'tween', 0.2, 1)}
-      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
       </motion.div>
